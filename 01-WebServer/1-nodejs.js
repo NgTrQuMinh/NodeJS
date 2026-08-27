d6bc8e33d85f5cb664433f24b6ee938862a0a975
Ng Tr Quang Minh <nguyentranquangminh207@gmail.com>
2026-08-27 11:22:15 +0700

    What is Node.JS

---
+++ 01-WebServer/1-nodejs.js
@@ -0,0 +1,35 @@
+/* What is Node.JS 
+1.Web server 
+Để viết (tạo ra) một web server, chúng ta cần ‘sử dụng’ các ngôn ngữ backend -  Java, PHP, Python, Ruby... và Javascript
+- Node.JS chính là công cụ (môi trường) giúp chúng ta có thể thực thi code Javascript trên Server.
+- Node.JS là platform (môi trường), không phải library (thư viện) hay framework. 
+
+2. Cài đặt Node.JS
+Download version mới nhất : https://nodejs.org/en/download/
+Sau khi cài đặt thành công Node.JS, công việc này sẽ: 
+- Cài đặt môi trường Node.JS 
+- Cài đặt NPM ứng với version Node.js đã cài đặt 
+ 
+Kiểm tra kết quả bằng cách sử dụng câu lệnh: 
+node -v  
+npm -v
+
+* NPM - Node Package Manager
+- NPM chính là công cụ quản lý các thư viện của Node.js
+Trang chủ: https://www.npmjs.com/ 
+
+Một vài câu lệnh thường dùng: 
+Tài liệu: https://docs.npmjs.com/cli/v8/commands/npm-install 
+ 
+Câu lệnh sử dụng nhiều nhất: 
+npm i  library_name 
+ 
+Hoặc  viết đầy đủ là npm install library_name 
+- file package.json 
+- 1 vài câu lệnh 
+ 
+
+* Hello world với node.js (chạy với node) 
+Để chạy file javascript, sử dụng câu lệnh: 
+node + File.js 
+*/
