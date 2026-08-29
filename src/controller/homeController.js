45ab909986baee9f0edd2555f811a54e25c7a3de
Ng Tr Quang Minh <nguyentranquangminh207@gmail.com>
2026-08-29 22:12:09 +0700

    Design NavBar NodeJS

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -3,19 +3,7 @@ const connection = require('../config/database');
 const getHomePage = (req, res) => {
     // Process Data
     // Call Model
-    let users = [];
-    connection.query(
-        'SELECT * FROM Users u',
-        function (err, result, fields) {
-            if (err) {
-                console.error('>>> Lỗi truy vấn: ', err);
-                return;
-            }
-            users = result;
-            console.log('>>>Result Home Page = ', result);
-            res.send(JSON.stringify(result));
-        }
-    );
+    return res.render('home.ejs');
 }
 
 const getNgTrQuMinh = (req, res) => {
